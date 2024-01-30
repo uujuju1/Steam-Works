c39b73c9f4d38287c3c3a915c64e5b2a57111c86
= <uujuju41@gmail.com>
2024-01-29 21:17:05 -0300

    turrets

--- src/sw/content/SWBlocks.java
+++ src/sw/content/SWBlocks.java
@@ -59,6 +59,7 @@ public class SWBlocks {
 		powerWire,
 		burner,
 
+		flow, trail, vniz, rozpad,
 		artyleriya, curve, thermikos,
 
 		ironWall, ironWallLarge, nickelWall, nickelWallLarge,
@@ -436,31 +437,197 @@ public class SWBlocks {
 		// endregion
 
 		// region turrets
-		artyleriya = new ItemTurret("artyleriya") {{
-			requirements(Category.turret, with(
-				SWItems.nickel, 100,
-				Items.graphite, 120
-			));
-			size = 2;
-			scaledHealth = 220f;
+		flow = new ItemTurret("flow") {{
+			requirements(Category.turret, with());
+			size = 1;
+			scaledHealth = 200;
 			reload = 60f;
-			range = 96f;
-			shoot = new ShootAlternate();
+			range = 140f;
+			targetAir = false;
+
+			shootSound = Sounds.shootAlt;
+
+			shoot = new ShootPattern() {{
+				shotDelay = 5f;
+				shots = 2;
+			}};
+
 			ammo(
-				SWItems.nickel, new BasicBulletType(2f, 30) {{
-					lifetime = 48f;
-					width = height = 12f;
-					splashDamage = 15;
-					splashDamageRadius = 16f;
+				SWItems.nickel, new BasicBulletType(2, 10) {{
+					frontColor = trailColor = Color.valueOf("A1A7AB");
+					backColor = Color.valueOf("595E61");
+					hitEffect = despawnEffect = Fx.hitBulletColor;
+					width = 8f;
+					height = 10f;
+					lifetime = 70f;
+					trailWidth = 1.5f;
+					trailLength = 5;
+					homingRange = 35f;
+					homingPower = 0.02f;
+					collidesAir = false;
 				}},
-				SWItems.iron, new ArtilleryBulletType(2f, 50) {{
-					lifetime = 60f;
-					rangeChange = 24f;
-					width = height = 12f;
-					splashDamage = 50;
-					splashDamageRadius = 24f;
+				SWItems.iron, new BasicBulletType(2, 15) {{
+					frontColor = trailColor = Color.valueOf("A1A1B8");
+					backColor = Color.valueOf("6F6F85");
+					hitEffect = despawnEffect = Fx.hitBulletColor;
+					width = 8f;
+					height = 10f;
+					lifetime = 70f;
+					trailWidth = 1.5f;
+					trailLength = 5;
+					homingRange = 35f;
+					homingPower = 0.03f;
+					collidesAir = false;
 				}}
 			);
+		}};
+		trail = new ItemTurret("trail") {{
+			requirements(Category.turret, with());
+			size = 2;
+			scaledHealth = 200;
+			reload = 30f;
+			range = 180f;
+			targetAir = false;
+
+			shoot = new ShootSummon(0f, 0f, 0f, 5f) {{
+				shots = 3;
+				shotDelay = 1f;
+			}};
+
+			ammo(
+				SWItems.nickel, new BasicBulletType(4, 15) {{
+					frontColor = trailColor = Color.valueOf("A1A7AB");
+					backColor = Color.valueOf("595E61");
+					hitEffect = despawnEffect = Fx.hitBulletColor;
+					width = 8f;
+					height = 10f;
+					lifetime = 45f;
+					trailWidth = 1.5f;
+					trailLength = 5;
+					homingRange = 35f;
+					homingPower = 0.04f;
+					collidesAir = false;
+				}},
+				SWItems.iron, new BasicBulletType(4, 20) {{
+					frontColor = trailColor = Color.valueOf("A1A1B8");
+					backColor = Color.valueOf("6F6F85");
+					hitEffect = despawnEffect = Fx.hitBulletColor;
+					width = 8f;
+					height = 10f;
+					lifetime = 45f;
+					trailWidth = 1.5f;
+					trailLength = 5;
+					homingRange = 35f;
+					homingPower = 0.04f;
+					collidesAir = false;
+				}}
+			);
+		}};
+		vniz = new ItemTurret("vniz") {{
+			requirements(Category.turret, with());
+			size = 1;
+			scaledHealth = 200;
+			reload = 90f;
+			range = 180f;
+			targetGround = false;
+
+			BulletType frag = new BasicBulletType(2f, 5) {{
+				frontColor = trailColor = Color.valueOf("A1A7AB");
+				backColor = Color.valueOf("595E61");
+				lifetime = 30f;
+				trailWidth = 1f;
+				trailLength = 5;
+				homingRange = 60f;
+				homingPower = 0.5f;
+				collidesGround = false;
+			}};
+			ammo(
+				SWItems.nickel, new ArtilleryBulletType(4, 10) {{
+					frontColor = trailColor = Color.valueOf("A1A7AB");
+					backColor = Color.valueOf("595E61");
+					hitEffect = despawnEffect = Fx.hitBulletColor;
+					width = 8f;
+					height = 10f;
+					lifetime = 45f;
+					trailWidth = 1.5f;
+					trailLength = 5;
+					collides = collidesAir = true;
+
+					fragBullets = 3;
+					fragBullet = frag;
+				}},
+				SWItems.iron, new ArtilleryBulletType(4, 15) {{
+					frontColor = trailColor = Color.valueOf("A1A1B8");
+					backColor = Color.valueOf("6F6F85");
+					hitEffect = despawnEffect = Fx.hitBulletColor;
+					width = 8f;
+					height = 10f;
+					lifetime = 45f;
+					trailWidth = 1.5f;
+					trailLength = 5;
+					collides = collidesAir = true;
+
+					fragBullets = 5;
+					fragBullet = frag;
+				}}
+			);
+		}};
+		rozpad = new ItemTurret("rozpad") {{
+			requirements(Category.turret, with());
+			size = 2;
+			scaledHealth = 200;
+			reload = 60f;
+			range = 200f;
+			targetGround = false;
+
+			shoot = new ShootAlternate();
+
+			BulletType frag = new BasicBulletType(2f, 10) {{
+				frontColor = trailColor = Color.valueOf("A1A7AB");
+				backColor = Color.valueOf("595E61");
+				lifetime = 30f;
+				trailWidth = 1f;
+				trailLength = 5;
+				homingRange = 60f;
+				homingPower = 0.5f;
+				collidesGround = false;
+			}};
+			ammo(
+				SWItems.nickel, new ArtilleryBulletType(4, 20) {{
+					frontColor = trailColor = Color.valueOf("A1A7AB");
+					backColor = Color.valueOf("595E61");
+					hitEffect = despawnEffect = Fx.hitBulletColor;
+					width = 8f;
+					height = 10f;
+					lifetime = 50f;
+					trailWidth = 1.5f;
+					trailLength = 5;
+					homingRange = 60f;
+					homingPower = 0.2f;
+					collides = collidesAir = true;
+
+					fragBullets = 3;
+					fragBullet = frag;
+				}},
+				SWItems.iron, new ArtilleryBulletType(4, 25) {{
+					frontColor = trailColor = Color.valueOf("A1A1B8");
+					backColor = Color.valueOf("6F6F85");
+					hitEffect = despawnEffect = Fx.hitBulletColor;
+					width = 8f;
+					height = 10f;
+					lifetime = 50f;
+					trailWidth = 1.5f;
+					trailLength = 5;
+					homingRange = 60f;
+					homingPower = 0.2f;
+					collides = collidesAir = true;
+
+					fragBullets = 5;
+					fragBullet = frag;
+				}}
+			);
+		}};
+		artyleriya = new ItemTurret("artyleriya") {{
 			researchCost = with(
 				SWItems.nickel, 200,
 				Items.graphite, 240
