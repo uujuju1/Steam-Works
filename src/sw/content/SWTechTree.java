97e7b70db89da7d0c42bd173d23b50b11e5bf1f5
Rinary <rinary.super@gmail.com>
2024-05-24 10:27:40 +0300

    Bigger Tech Tree

--- src/sw/content/SWTechTree.java
+++ src/sw/content/SWTechTree.java
@@ -11,6 +11,9 @@ import static sw.content.blocks.SWDefense.*;
 import static sw.content.blocks.SWDistribution.*;
 import static sw.content.blocks.SWProduction.*;
 import static sw.content.blocks.SWTurrets.*;
+import static sw.content.blocks.SWPower.*;
+import static sw.content.blocks.SWCrafting.*;
+import static sw.content.SWLiquids.*;
 
 public class SWTechTree {
   public static TechNode root;
@@ -19,22 +22,46 @@ public class SWTechTree {
     root = nodeRoot("Steam Works", coreScaffold, () -> {
       // region items
       nodeProduce(nickel, () -> {
-        nodeProduce(iron, () -> {
-          nodeProduce(Items.silicon, () -> {});
-        });
+        nodeProduce(iron, () -> {});
+		nodeProduce(Items.graphite, () -> {
+			nodeProduce(Items.silicon, () -> {
+                nodeProduce(compound, () -> {
+					nodeProduce(chalk, () -> {});
+					nodeProduce(dense-alloy, () -> {
+						nodeProduce(thermite, () -> {});
+					});
+				});
+            });
+		});
+		nodeProduce(Items.sand, () -> {});
       });
       // endregion
+	  // region liquids
+	  nodeProduce(solvent, () -> {
+		  nodeProduce(SWLiquids.steam, () -> {});
+		  nodeProduce(chloro, () -> {});
+		  nodeProduce(fungi, () -> {});
+	  });
+	  // endregion
       // region defense
       node(nickelWall, () -> {
         node(nickelWallLarge);
         node(ironWall, () -> {
           node(ironWallLarge);
         });
+		node(compoundWall, () -> {
+			node(compoundWallLarge);
+		});
+		node(denseWall, () -> {
+			node(denseWallLarge);
+		});
 
         node(flow, () -> {
           node(trail);
           node(vniz, () -> {
-            node(rozpad);
+            node(rozpad, () -> {
+				node(sonar);
+			});
           });
         });
       });
@@ -62,11 +89,38 @@ public class SWTechTree {
       });
       // endregion
       // region crafting
-      node(SWCrafting.siliconBoiler);
+      node(SWCrafting.siliconBoiler, () -> {
+		  node(SWCrafting.compoundSmelter, () -> {
+			  node(SWCrafting.densePress, () -> {
+				  node(SWCrafting.thermiteMixer);
+			  });
+			  node(SWCrafting.chalkSeparator);
+		  });
+	  });
       // endregion
-	    // region sectors
-	    node(intro, () -> {});
-	    // endreigon
+	  // region sectors
+	  node(intro, () -> {});
+	  // endreigon
+	  // region power
+	  node(SWPower.combustionTensionGenerator, () -> {
+		  node(SWPower.lowWire, () -> {
+			  node(SWPower.lowRouter, () -> {
+				  node(SWPower.coatedRouter);
+			  });
+			  node(SWPower.lowBridge, () -> {
+				  node(SWPower.coatedBridge);
+			  });
+			  node(SWPower.coatedWire);
+			  node(SWPower.wireAdapter);
+			  node(SWPower.wireJunction);
+		  });
+	  });
+	  // endregion
+	  // region Units
+	  node(SWBlocks.constructor, () -> {
+		  node(SWBlocks.upgrader);
+	  });
+	  //endregion
     });
     SWPlanets.wendi.techTree = root;
   }
