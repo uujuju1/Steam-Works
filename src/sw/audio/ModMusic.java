3c43eaadf89d6f335f6d026e3ce257496437ae9b
= <uujuju41@gmail.com>
2024-07-10 22:47:37 -0300

    i'll do this thing before i forget to do it when i'm publishing the release

--- src/sw/audio/ModMusic.java
+++ src/sw/audio/ModMusic.java
@@ -6,12 +6,13 @@ import arc.struct.*;
 import static mindustry.Vars.*;
 
 public class ModMusic {
-	public static Music adventurers, steelSoul, descend;
+	public static Music adventurers, steelSoul, descend, cigarra;
 
 	public static void load() {
 		adventurers = loadMusic(tree.loadMusic("adventurers"), control.sound.ambientMusic);
 		steelSoul = loadMusic(tree.loadMusic("steelsoul"), control.sound.darkMusic);
-		descend = loadMusic(tree.loadMusic("descend"), control.sound.ambientMusic);
+		descend = loadMusic(tree.loadMusic("descend"), control.sound.darkMusic);
+		cigarra = loadMusic(tree.loadMusic("cigarra"), control.sound.ambientMusic);
 	}
 
 	public static Music loadMusic(Music music, Seq<Music> to) {
