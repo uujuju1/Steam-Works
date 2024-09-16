7ae1bf173c57b097d9ef585d77cab161091ee8a3
= <uujuju41@gmail.com>
2024-09-15 21:12:22 -0300

    remove unknown

--- src/sw/content/SWPlanets.java
+++ src/sw/content/SWPlanets.java
@@ -1,18 +1,15 @@
 package sw.content;
 
-import arc.math.geom.*;
 import mindustry.content.*;
 import mindustry.game.*;
 import mindustry.graphics.*;
 import mindustry.graphics.g3d.*;
 import mindustry.type.*;
 import sw.content.blocks.*;
-import sw.dream.*;
-import sw.maps.*;
 import sw.maps.generators.*;
 
 public class SWPlanets {
-	public static Planet wendi, unknown;
+	public static Planet wendi;
 
 	public static void load() {
 		wendi = new Planet("wendi", Planets.sun, 1f, 3) {{
@@ -53,28 +50,6 @@ public class SWPlanets {
 				baseColor = SWEnvironment.soil.mapColor;
 				defaultLoadout = Schematics.readBase64("bXNjaAF4nGNgZmBmZmDJS8xNZeBNSizOTA5OTkxLy89JYeBOSS1OLsosKMnMz2NgYGDLSUxKzSlmYIqOZWQQKC7XTc4vStUthqlmYGAEISABAKHWFQU=");
 			}};
-		}};
-		unknown = new Planet("unknown", Planets.sun, 1f, 1) {{
-			defaultCore = DreamContent.brutalistCore;
-			hasAtmosphere = updateLighting = false;
-			clearSectorOnLose = true;
-			meshLoader = () -> new HexMesh(this, 7);
-			generator = new ModularPlanetGenerator() {{
-				defaultLoadout = Schematics.readBase64("bXNjaAF4nGNgYmBiZmDJS8xNZeB1zi9KjUxPTylKLM7MYeBOSS1OLsosKMnMz2NgYGDLSUxKzSlmYIqOZWQQLC7XTSoqLUnMySwu0U0G6gOqYGQAA0YAwcYVpg==");
-				baseColor = DreamContent.voidTile.mapColor;
-				sectors.each(sector -> {
-					heights.add(new HeightPass.SphereHeight() {{
-						pos.set(sector.tile.v);
-						radius = 0.1f;
-						offset = 0.5f;
-						set = true;
-					}});
-					colors.add(new ColorPass.SphereColorPass(new Vec3(sector.tile.v), 0.15f, DreamContent.concrete.mapColor));
-				});
-			}};
-			drawOrbit = false;
-			orbitRadius = 1000f;
-			solarSystem = this;
 		}};
 	}
 }
