48e8abf4b18998e125fca0fd78e5442f3e81e889
= <uujuju41@gmail.com>
2024-10-07 17:24:29 -0300

    items

--- src/sw/content/SWItems.java
+++ src/sw/content/SWItems.java
@@ -6,9 +6,9 @@ import static arc.graphics.Color.*;
 
 public class SWItems {
   public static Item
-    nickel, iron,
-    compound, chalk,
-    denseAlloy, thermite;
+    nickel, iron, meteorite,
+    compound, chalk, soda,
+    denseAlloy, thermite, residue;
 
   public static void load() {
     nickel = new Item("nickel", valueOf("CCD4D9")) {{
@@ -17,6 +17,7 @@ public class SWItems {
     iron = new Item("iron", valueOf("646485")) {{
       hardness = 2;
     }};
+    meteorite = new Item("meteorite", valueOf("E4CAAC"));
 
     compound = new Item("compound", valueOf("515151")) {{
       cost = 1f;
@@ -24,6 +25,7 @@ public class SWItems {
     chalk = new Item("chalk", valueOf("E2E2E8")) {{
       cost = 2f;
     }};
+    soda = new Item("soda", valueOf("C9C3E4"));
 
     denseAlloy = new Item("dense-alloy", valueOf("6E7080")) {{
       cost = 1f;
@@ -32,5 +34,6 @@ public class SWItems {
       cost = 2f;
       flammability = explosiveness = 0.8f;
     }};
+    residue = new Item("residue", valueOf("1C1C1C"));
   }
 }
