f84fd0db0c91c5c2a4651ca18cca07bebdaa2c7c
= <uujuju41@gmail.com>
2025-04-12 14:35:36 -0300

    version change

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,9 @@ buildscript{
 
     repositories{
         if(!useJitpack) maven("https://raw.githubusercontent.com/Zelaux/MindustryRepo/master/repository")
+
+        maven("https://maven.xpdustry.com/mindustry")
+
         maven("https://jitpack.io")
     }
 }
@@ -82,6 +85,9 @@ allprojects{
 
         // Use Zelaux's non-buggy repository for release Mindustry and Arc builds.
         if(!useJitpack) maven("https://raw.githubusercontent.com/Zelaux/MindustryRepo/master/repository")
+
+        maven("https://maven.xpdustry.com/mindustry")
+
         maven("https://jitpack.io")
     }
 
