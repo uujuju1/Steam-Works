9fbcfec18a1f952e3b1fb3fbaa269391cc0b0847
= <uujuju41@gmail.com>
2025-04-17 17:51:15 -0300

    graph is now working again

---
+++ src/sw/entities/units/SpinGraphUpdater.java
@@ -0,0 +1,96 @@
+package sw.entities.units;
+
+import arc.util.*;
+import arc.util.io.*;
+import mindustry.gen.*;
+import sw.world.graph.*;
+
+@SuppressWarnings("unchecked")
+public class SpinGraphUpdater implements Entityc {
+	public boolean added;
+	public int id;
+	public SpinGraph graph;
+
+	@Override
+	public int classId() {
+		return 0;
+	}
+
+	public SpinGraphUpdater setGraph(SpinGraph newGraph) {
+		graph = newGraph;
+		return self();
+	}
+
+	@Override
+	public int id() {
+		return id;
+	}
+
+	@Override
+	public void add() {
+		if (!added) {
+			id = Groups.all.addIndex(this);
+			added = true;
+		}
+	}
+
+	@Override
+	public void remove() {
+		if (added) {
+			Groups.all.remove(this);
+			added = false;
+		}
+	}
+
+	@Override
+	public void update() {
+		if (graph != null) {
+			graph.update();
+		} else {
+			Log.err("Graph is null. Removing.");
+			remove();
+		}
+	}
+
+
+	@Override public void id(int i) {
+
+	}
+
+	@Override public <T extends Entityc> T self() {
+		return (T) this;
+	}
+	@Override public <T> T as() {
+		return (T) this;
+	}
+
+	@Override public boolean isAdded() {
+		return false;
+	}
+	@Override public boolean isLocal() {
+		return false;
+	}
+	@Override public boolean isRemote() {
+		return false;
+	}
+
+	@Override public boolean serialize() {
+		return false;
+	}
+
+	@Override public void afterRead() {
+
+	}
+	@Override public void afterReadAll() {
+
+	}
+	@Override public void beforeWrite() {
+
+	}
+	@Override public void read(Reads reads) {
+
+	}
+	@Override public void write(Writes writes) {
+
+	}
+}
--- src/sw/world/graph/SpinGraph.java
+++ src/sw/world/graph/SpinGraph.java
@@ -4,7 +4,7 @@ import arc.math.*;
 import arc.struct.*;
 import arc.util.*;
 import mindustry.content.*;
-//import sw.gen.*;
+import sw.entities.units.*;
 import sw.world.interfaces.*;
 
 /**
@@ -27,7 +27,7 @@ public class SpinGraph {
 	/**
 	 * Entity of this graph.
 	 */
-//	public final SpinGraphUpdater updater = SpinGraphUpdater.create().setGraph(this);
+	public final SpinGraphUpdater updater = new SpinGraphUpdater().setGraph(this);
 
 	/**
 	 * List of buildings of this graph.
@@ -64,9 +64,9 @@ public class SpinGraph {
 	 */
 	public void checkEntity() {
 		if (builds.size > 0) {
-//			updater.add();
+			updater.add();
 		} else {
-//			updater.remove();
+			updater.remove();
 		}
 	}
 
