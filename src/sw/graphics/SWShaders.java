92e8edbdf1cd4db16b751bf839c26d6d9fab1230
= <uujuju41@gmail.com>
2025-05-03 20:31:39 -0300

    remove chasm shader

--- src/sw/graphics/SWShaders.java
+++ src/sw/graphics/SWShaders.java
@@ -13,11 +13,10 @@ import sw.*;
 
 public class SWShaders {
 	public static PitfallShader pitfall;
-	public static ChasmShader chasm;
 
 	public static UIShader sectorDialogBackground, techtreeBackground;
 
-	public static CacheLayer pitfallLayer, chasmLayer;
+	public static CacheLayer pitfallLayer;
 
 	public static void load() {
 		sectorDialogBackground = new UIShader("sectorDialogBackground");
@@ -26,10 +25,6 @@ public class SWShaders {
 		pitfall = new PitfallShader("pitfall");
 		pitfallLayer = new CacheLayer.ShaderLayer(pitfall);
 		CacheLayer.add(pitfallLayer);
-
-		chasm = new ChasmShader("chasm");
-		chasmLayer = new CacheLayer.ShaderLayer(chasm);
-		CacheLayer.add(chasmLayer);
 	}
 
 	public static class UIShader extends Shader {
@@ -46,74 +41,6 @@ public class SWShaders {
 			setUniformf("u_position", pos.x, pos.y);
 			setUniformf("u_opacity", alpha);
 			setUniformf("u_time", Time.globalTime);
-		}
-	}
-
-	public static class ChasmShader extends Shader {
-		TextureRegion toplayer;
-		TextureRegion bottomlayer;
-		TextureRegion truss;
-
-		Texture noiseTex;
-
-		String toplayerName = "sw-concrete-stripes1";
-		String bottomlayerName = "sw-concrete1";
-		String trussName = "sw-void-tile-edge";
-		String noiseTexName = "noise";
-
-		public float sampleLen = 20;
-		public float epsilonp1 = 1.01f;
-
-		public ChasmShader(String frag) {
-			super(Core.files.internal("shaders/screenspace.vert"), Vars.tree.get("shaders/" + frag + ".frag"));
-			loadNoise();
-		}
-
-		@Override
-		public void apply(){
-			var texture = Core.atlas.find("grass1").texture;
-			if(toplayer == null){
-				toplayer = Core.atlas.find(toplayerName);
-			}
-			if(bottomlayer == null){
-				bottomlayer = Core.atlas.find(bottomlayerName);
-			}
-			if(truss == null){
-				truss = Core.atlas.find(trussName);
-			}
-			if(noiseTex == null){
-				noiseTex = Core.assets.get("sprites/" + textureName() + ".png", Texture.class);
-			}
-			setUniformf("u_campos", Core.camera.position.x - Core.camera.width / 2, Core.camera.position.y - Core.camera.height / 2);
-			setUniformf("u_resolution", Core.camera.width, Core.camera.height);
-			setUniformf("u_time", Time.time);
-//tvariants
-			setUniformf("u_toplayer", toplayer.u, toplayer.v, toplayer.u2, toplayer.v2);
-			setUniformf("u_bottomlayer", bottomlayer.u, bottomlayer.v, bottomlayer.u2, bottomlayer.v2);
-			setUniformf("bvariants", bottomlayer.width/32f);
-			setUniformf("u_truss", truss.u, truss.v, truss.u2, truss.v2);
-
-			setUniformf("u_maskSize", SWVars.renderer.pitfall.width, SWVars.renderer.pitfall.height);
-
-			setUniformf("samplelen", sampleLen);
-			setUniformf("epsilonp1", epsilonp1);
-
-			texture.bind(2);
-			noiseTex.bind(1);
-			SWVars.renderer.pitfall.bind(0);
-			setUniformi("u_noise", 1);
-			setUniformi("u_texture2", 2);
-		}
-
-		public void loadNoise(){
-			Core.assets.load("sprites/" + textureName() + ".png", Texture.class).loaded = t -> {
-				t.setFilter(Texture.TextureFilter.linear);
-				t.setWrap(Texture.TextureWrap.repeat);
-			};
-		}
-
-		public String textureName(){
-			return noiseTexName;
 		}
 	}
 
