3e7242ee06c08fb025f29dc04d442ffe2f575882
= <uujuju41@gmail.com>
2026-01-09 19:31:17 -0300

    thingy

--- src/sw/world/blocks/power/AxleBrake.java
+++ src/sw/world/blocks/power/AxleBrake.java
@@ -1,17 +1,42 @@
 package sw.world.blocks.power;
 
+import arc.*;
+import arc.math.*;
+import arc.util.*;
+import sw.world.meta.*;
+
 public class AxleBrake extends AxleBlock {
-	public float targetSpeed = 10;
+	public float beginBrakeSpeed = 1f, endBrakeSpeed = 2f;
 	
-	public float brakeTarget = 10;
+	public float brakeStrength = 1f / 600f;
 	
 	public AxleBrake(String name) {
 		super(name);
 	}
 	
+	@Override
+	public void init() {
+		super.init();
+		
+		if (spinConfig != null) {
+			spinConfig.checkSpeed = false;
+		}
+	}
+	
+	@Override
+	public void setStats() {
+		super.setStats();
+		stats.add(SWStat.spinRequirement, Core.bundle.format(
+			"stat.sw-spin-requirement.format",
+			Strings.autoFixed(beginBrakeSpeed * 600f, 2),
+			Strings.autoFixed(endBrakeSpeed * 600f, 2)
+		));
+		stats.add(SWStat.spinOutputForce, brakeStrength * 600f, SWStat.force);
+	}
+	
 	public class AxleBrakeBuild extends AxleBlockBuild {
-		@Override public float getResistance() {
-			return super.getResistance() + (getRotation() > targetSpeed && efficiency > 0 ? brakeTarget : 0f);
+		@Override public float getForce() {
+			return Mathf.clamp(Mathf.map(getRotation(), beginBrakeSpeed, endBrakeSpeed, 0f, 1f)) * -brakeStrength;
 		}
 	}
 }
