2b270088d0151369ebea75f7a20664e56fc024ef
Liz <j4224253@gmail.com>
2026-08-08 23:47:46 -0300

    finished thing kinda now to make content

--- src/sw/matryoshka/Tutorials.java
+++ src/sw/matryoshka/Tutorials.java
@@ -1,8 +1,13 @@
 package sw.matryoshka;
 
+import arc.graphics.g2d.*;
+import arc.math.*;
 import arc.scene.ui.layout.*;
+import mindustry.*;
 import mindustry.content.*;
+import mindustry.entities.*;
 import mindustry.game.*;
+import mindustry.graphics.*;
 import mindustry.ui.*;
 import sw.*;
 import sw.content.*;
@@ -12,6 +17,7 @@ import sw.matryoshka.ui.*;
 import sw.matryoshka.world.*;
 import sw.world.blocks.power.*;
 import sw.world.blocks.sandbox.*;
+import sw.world.interfaces.*;
 
 public class Tutorials {
 	public static TutorialSequence demo = new TutorialBuilder() {{
@@ -64,8 +70,9 @@ public class Tutorials {
 				belt2.incoming.add(belt1.pos());
 			});
 
-			maxTime = 2000f;
+			maxTime = 2500f;
 
+			// region spin blocks
 			timestamp.add(100f);
 			actions.add(new UIElementAction(new Table(t -> {
 				t.setFillParent(true);
@@ -76,7 +83,9 @@ public class Tutorials {
 				startTime = 120f;
 				endTime = 360f;
 			}});
+			// endregion
 
+			// region spin buildings
 			timestamp.add(400f);
 			nesting.actions.add(new RunnablesAction() {{
 				startTime = 420f;
@@ -97,8 +106,8 @@ public class Tutorials {
 					nesting.world.tile(4, 3).setBlock(SWCrafting.engineSmelter, Team.sharded, 0);
 					nesting.world.tile(4, 5).setBlock(SWProduction.mechanicalFracker, Team.sharded, 1);
 					nesting.world.tile(4, 1).setBlock(SWSandbox.carnotEngine, Team.sharded, 1);
-					SWFx.changeEffect.at(32, 56, 0, SWCrafting.engineSmelter);
-					SWFx.changeEffect.at(40, 56, 0, SWCrafting.engineSmelter);
+					SWFx.changeEffect.at(32, 56, 0, Blocks.router);
+					SWFx.changeEffect.at(40, 56, 0, Blocks.router);
 					SWFx.changeEffect.at(32, 24, 0, SWCrafting.engineSmelter);
 					SWFx.changeEffect.at(4.5f * 8f, 5.5f * 8, 0, SWProduction.mechanicalFracker);
 					SWFx.changeEffect.at(32f, 8f, 0, SWSandbox.carnotEngine);
@@ -117,6 +126,201 @@ public class Tutorials {
 				startTime = 450f;
 				endTime = 660f;
 			}});
+			// endregion
+
+			// region spin edges
+			timestamp.add(660f);
+			nesting.actions.add(new RunnablesAction() {{
+				startTime = 690f;
+				endTime = 720f;
+				start = (ignored, ignoredToo) -> {
+					nesting.world.tiles.eachTile(tile -> {
+						if (tile.block() != Blocks.air) {
+							SWFx.changeEffect.at(tile.build == null ? tile.drawx() : tile.build.x, tile.build == null ? tile.drawy() : tile.build.y, 0, tile.block());
+							tile.setAir();
+						}
+						if (tile.overlay() != Blocks.air) tile.setOverlay(Blocks.air);
+					});
+				};
+				end = (ignored, ignoredToo) -> {
+					nesting.world.tile(2, 2).setBlock(SWPower.wireShaftRouter, Team.sharded, 0);
+					nesting.world.tile(2, 6).setBlock(SWPower.wireShaft, Team.sharded, 0);
+					nesting.world.tile(6, 2).setBlock(SWPower.handWheel, Team.sharded, 0);
+					nesting.world.tile(6, 6).setBlock(SWCrafting.engineSmelter, Team.sharded, 0);
+					SWFx.changeEffect.at(16, 16, 0, SWPower.wireShaftRouter);
+					SWFx.changeEffect.at(16, 48, 0, SWPower.wireShaft);
+					SWFx.changeEffect.at(48, 16, 0, SWPower.handWheel);
+					SWFx.changeEffect.at(48, 48, 0, SWCrafting.engineSmelter);
+				};
+			}});
+			actions.add(new UIElementAction(new Table(t -> {
+				t.setFillParent(true);
+//				t.left();
+				t.table(Styles.black6, table -> table.add("@tutorial.sw-spin-transmission.spin-edges").pad(10f));
+			})) {{
+				fadeSeconds = 0.5f;
+				startTime = 720f;
+				endTime = 1020f;
+			}});
+			nesting.actions.add(new RunnablesAction() {{
+				startTime = endTime = 840f;
+				start = (ignored, ignoredToo) -> {
+					Effect effect = new Effect(180f, e -> {
+						if (e.data instanceof HasSpin a) {
+							Draw.alpha(e.foutpowdown());
+							a.getConnectingOuterEdges().each(p -> Draw.rect(
+								"sw-icon-spin-edge",
+								(a.asBuilding().tileX() + p.x + (p.x == 0 ? 0 : -Mathf.sign(p.x) / 2f)) * Vars.tilesize,
+								(a.asBuilding().tileY() + p.y + (p.y == 0 ? 0 : -Mathf.sign(p.y) / 2f)) * Vars.tilesize,
+								4, 4
+							));
+						}
+					}).layer(Layer.effect + 1f);
+					effect.at(0, 0, 0, nesting.world.build(2, 2));
+					effect.at(0, 0, 0, nesting.world.build(2, 6));
+					effect.at(0, 0, 0, nesting.world.build(6, 2));
+					effect.at(0, 0, 0, nesting.world.build(6, 6));
+				};
+			}});
+			// endregion
+
+			// region edge pair
+			timestamp.add(1080f);
+			nesting.actions.add(new RunnablesAction() {{
+				startTime = 1110f;
+				endTime = 1140f;
+				start = (ignored, ignoredToo) -> {
+					nesting.world.tiles.eachTile(tile -> {
+						if (tile.build != null) {
+							SWFx.changeEffect.at(tile.build.x, tile.build.y, 0, tile.build.block);
+							tile.setAir();
+						}
+					});
+				};
+				end = (ignored, ignoredToo) -> {
+					for (int i = 1; i < 8; i++) {
+						nesting.world.tile(i, 4).setBlock(SWPower.wireShaft, Team.sharded, 0);
+						SWFx.changeEffect.at(i * Vars.tilesize, 32, 0, SWPower.wireShaft);
+					}
+					nesting.world.tile(1, 4).setBlock(SWSandbox.carnotEngine, Team.sharded, 0);
+
+					var source = ((SpinSource.SpinSourceBuild) nesting.world.build(1, 4));
+					source.targetSpeed = 2f;
+					source.force = 1f;
+				};
+			}});
+			actions.add(new UIElementAction(new Table(t -> {
+				t.setFillParent(true);
+//				t.left();
+				t.table(Styles.black6, table -> table.add("@tutorial.sw-spin-transmission.edge-pair").pad(10f));
+			})) {{
+				fadeSeconds = 0.5f;
+				startTime = 1200f;
+				endTime = 1380f;
+			}});
+			nesting.actions.add(new RunnablesAction() {{
+				startTime = endTime = 1440f;
+				start = (ignored, ignoredToo) -> {
+					new Effect(120f, e -> {
+						if (e.data instanceof HasSpin a) {
+							Draw.color(Pal.heal);
+							a.getConnectingOuterEdges().each(p -> Draw.rect(
+								"sw-icon-spin-edge",
+								(a.asBuilding().tileX() + p.x + (p.x == 0 ? 0 : -Mathf.sign(p.x) / 2f)) * Vars.tilesize,
+								(a.asBuilding().tileY() + p.y + (p.y == 0 ? 0 : -Mathf.sign(p.y) / 2f)) * Vars.tilesize,
+								4, 4
+							));
+						}
+					}).layer(Layer.effect + 1f).at(0, 0, 0, nesting.world.build(4, 4));
+				};
+			}});
+			nesting.actions.add(new RunnablesAction() {{
+				startTime = endTime = 1560f;
+				start = (ignored, ignoredToo) -> {
+					nesting.world.tile(4, 4).setBlock(SWPower.wireShaft, Team.sharded, 1);
+					SWFx.changeEffect.at(32, 32, 0, SWPower.wireShaft);
+					new Effect(120f, e -> {
+						if (e.data instanceof HasSpin a) {
+							Draw.color(Pal.remove);
+							Draw.alpha(e.foutpowdown());
+							a.getConnectingOuterEdges().each(p -> Draw.rect(
+								"sw-icon-spin-edge",
+								(a.asBuilding().tileX() + p.x + (p.x == 0 ? 0 : -Mathf.sign(p.x) / 2f)) * Vars.tilesize,
+								(a.asBuilding().tileY() + p.y + (p.y == 0 ? 0 : -Mathf.sign(p.y) / 2f)) * Vars.tilesize,
+								4, 4
+							));
+						}
+					}).layer(Layer.effect + 1f).at(0, 0, 0, nesting.world.build(4, 4));
+				};
+			}});
+			// endregion
+
+			// region exceptions
+
+			timestamp.add(1860f);
+			nesting.actions.add(new RunnablesAction() {{
+				startTime = 1890f;
+				endTime = 1920f;
+				start = (ignored, ignoredToo) -> {
+					nesting.world.tiles.eachTile(tile -> {
+						if (tile.build != null) {
+							SWFx.changeEffect.at(tile.build.x, tile.build.y, 0, tile.build.block);
+							tile.setAir();
+						}
+					});
+				};
+				end = (ignored, ignoredToo) -> {
+					nesting.world.tile(3, 5).setBlock(SWSandbox.carnotEngine, Team.sharded, 0);
+					nesting.world.tile(4, 5).setBlock(SWPower.wireShaft, Team.sharded, 1);
+					nesting.world.tile(3, 3).setBlock(SWPower.evaporator, Team.sharded, 0);
+					SWFx.changeEffect.at(24, 40, 0, SWSandbox.carnotEngine);
+					SWFx.changeEffect.at(32, 40, 0, SWPower.wireShaft);
+					SWFx.changeEffect.at(28, 28, 0, SWPower.evaporator);
+
+					var source = ((SpinSource.SpinSourceBuild) nesting.world.build(3, 5));
+					source.targetSpeed = 2f;
+					source.force = 1f;
+				};
+			}});
+			actions.add(new UIElementAction(new Table(t -> {
+				t.setFillParent(true);
+//				t.left();
+				t.table(Styles.black6, table -> table.add("@tutorial.sw-spin-transmission.exceptions").pad(10f));
+			})) {{
+				fadeSeconds = 0.5f;
+				startTime = 1920f;
+				endTime = 2100f;
+			}});
+			nesting.actions.add(new RunnablesAction() {{
+				startTime = endTime = 2130f;
+				start = (ignored, ignoredToo) -> {
+					nesting.world.tiles.eachTile(tile -> {
+						for (int i = 0; i < 3; i++) {
+							nesting.world.tile(i * 2 + 1, 5).setBlock(SWSandbox.carnotEngine, Team.sharded, 0);
+							nesting.world.tile(i * 2 + 2, 5).setBlock(SWPower.wireShaft, Team.sharded, 1);
+							nesting.world.tile(i * 2 + 1, 3).setBlock(SWPower.evaporator, Team.sharded, 0);
+							SWFx.changeEffect.at((i * 2 + 1) * Vars.tilesize, 40, 0, SWSandbox.carnotEngine);
+							SWFx.changeEffect.at((i * 2 + 2) * Vars.tilesize, 40, 0, SWPower.wireShaft);
+							SWFx.changeEffect.at((i * 2 + 1.5f) * Vars.tilesize, 28, 0, SWPower.evaporator);
+
+							var source = ((SpinSource.SpinSourceBuild) nesting.world.build(i * 2 + 1, 5));
+							source.targetSpeed = 2f + i * 2;
+							source.force = 1f;
+						}
+					});
+				};
+			}});
+			actions.add(new UIElementAction(new Table(t -> {
+				t.setFillParent(true);
+//				t.left();
+				t.table(Styles.black6, table -> table.add("@tutorial.sw-spin-transmission.exception-pair").pad(10f));
+			})) {{
+				fadeSeconds = 0.5f;
+				startTime = 2130f;
+				endTime = 2370f;
+			}});
+
+			// endregion
 		}});
 	}};
 }
